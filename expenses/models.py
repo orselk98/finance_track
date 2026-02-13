34978a1cd02d4d8253bd37d104ab121840c81b6a
Orsel Kapllani <orsel.a.kapllani@gmail.com>
2026-02-13 22:57:41 +0100

    Add expense tracker models:category and Expense with initial data migration

---
+++ expenses/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+    amount =models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.CharField(max_length=100)
+    date=models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.description} - {self.amount}"
+
+
+
