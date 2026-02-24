bcf8cd77dfb912f2df141ebc2f6c65a2e4200745
Orsel Kapllani <orsel.a.kapllani@gmail.com>
2026-02-24 23:22:33 +0100

    Add _transaction_detail endpoint

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CreditCardViewset, all_transactions
+from .views import CreditCardViewset, all_transactions, transaction_detail
 
 router =DefaultRouter()
 router.register(r'credit_cards', CreditCardViewset)
@@ -8,4 +8,5 @@ router.register(r'credit_cards', CreditCardViewset)
 urlpatterns = [
     path('', include(router.urls)),
     path('transactions/', all_transactions, name='all-transactions'),
+    path('transaction-detail/<int:pk>/', transaction_detail, name='transaction-detail'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from expenses.serializers import CreditCardSerializer , TransactionSerializer
 from .models import CreditCard, Transaction
 from rest_framework.response import Response
+
 
 # TODO: Rebuild views
 
@@ -23,14 +25,24 @@ def all_transactions(request):
             serializer.save()
             return Response(serializer.data)
         return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-def get_transaction(request,pk):
-    try:
-        transaction=Transaction.objects.get(pk=pk)
-    except Transaction.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
     
-    if request.method =="GET":
-        serializer=TransactionSerializer(transaction)
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def transaction_detail(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == 'GET':
+        
+        serializer = TransactionSerializer(transaction, many=False)
         return Response(serializer.data)
+    
+        
+    if request.method == 'PATCH':
+        serializer = TransactionSerializer(transaction, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        transaction.delete()
+        return Response({"message": "Transaction deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
