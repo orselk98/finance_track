68fd7225f25da5a22789ac27f1daba2898716c7e
Orsel Kapllani <orsel.a.kapllani@gmail.com>
2026-02-27 22:38:00 +0100

    Added transaction_stats_by_category endpoint

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CreditCardViewset, all_transactions, transaction_detail, transactions_list
+from .views import CreditCardViewset, all_transactions, transaction_detail, transaction_stats_by_category, transactions_list
 
 router =DefaultRouter()
 router.register(r'credit_cards', CreditCardViewset)
@@ -10,4 +10,5 @@ urlpatterns = [
     path('transactions/', all_transactions, name='all-transactions'),
     path('transaction-detail/<int:pk>/', transaction_detail, name='transaction-detail'),
     path('transactions-list/', transactions_list, name='transactions-list'),
+    path('transaction-stats-by-category/', transaction_stats_by_category, name='transaction-stats-by-category'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from expenses.serializers import CreditCardSerializer , TransactionSerializer
 from .models import CreditCard, Transaction
 from rest_framework.response import Response
+from django.db.models import Avg
 
 
 # TODO: Rebuild views
@@ -72,3 +73,7 @@ def transactions_list(request):
     return Response(serializer.data)
 
 
+@api_view(['GET'])
+def transaction_stats_by_category(request):
+    qs=Transaction.objects.values('category').annotate(average_amount=Avg('amount'))
+    return Response(list(qs))
